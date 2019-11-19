a80e47b0beb1761c2a10b07f7870cf13c33089d7
santoshreddy254 <santoshreddy45@yahoo.com>
2019-11-19 18:54:42 +0100

    test cases written, name format for backgrounds changed

--- src/gui_testing.py
+++ src/gui_testing.py
@@ -3,14 +3,17 @@ import os
 import AIG_Window
 from time import sleep
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5 import QtGui
 from PyQt5.QtGui import *
 import pytest
 import pytestqt
+from glob import glob
+import cv2
 
-def test_gui(qtbot):
-    # app = QApplication(sys.argv)
+
+def test_gui_1(qtbot):
+    app = QApplication(sys.argv)
     window = AIG_Window.MainWindow()
     qtbot.addWidget(window)
     window.num_images.setText(str(2))
@@ -19,9 +22,39 @@ def test_gui(qtbot):
     window.label_folder.setText(cwd+"/semantic_labels")
     window.backgrounds_folder.setText(cwd+"/backgrounds")
     window.labels_file_path.setText(cwd+"/labels.txt")
-    window.show()
+    window.image_save_path.setText(cwd)
     qtbot.waitForWindowShown(window)
-    print(window.button1.isEnabled())
-
     assert window.button1.isEnabled() == True
     qtbot.mouseClick(window.button1, Qt.LeftButton)
+    assert os.path.exists(
+        cwd+"/augmented/images/") == True and len(os.listdir(cwd+"/augmented/images/")) == 2
+    assert os.path.exists(
+        cwd+"/augmented/labels/") == True and len(os.listdir(cwd+"/augmented/labels/")) == 2
+    assert os.path.exists(cwd+"/augmented/masks/") == True
+    assert os.path.exists(cwd+"/augmented/obj_det_label/") == True
+
+
+def test_gui_2(qtbot):
+    app = QApplication(sys.argv)
+    window = AIG_Window.MainWindow()
+    qtbot.addWidget(window)
+    window.num_images.setText(str(2))
+    cwd = os.getcwd()
+    window.image_folder.setText(cwd+"/images")
+    window.label_folder.setText(cwd+"/semantic_labels")
+    window.backgrounds_folder.setText(cwd+"/backgrounds")
+    window.labels_file_path.setText(cwd+"/labels.txt")
+    window.image_save_path.setText(cwd)
+    window.rbutton3.setChecked(True)
+    window.rbutton1.setChecked(True)
+    qtbot.waitForWindowShown(window)
+    assert window.button1.isEnabled() == True
+    qtbot.mouseClick(window.button1, Qt.LeftButton)
+    assert os.path.exists(
+        cwd+"/augmented/images/") == True and len(os.listdir(cwd+"/augmented/images/")) == 2
+    assert os.path.exists(
+        cwd+"/augmented/labels/") == True and len(os.listdir(cwd+"/augmented/labels/")) == 2
+    assert os.path.exists(
+        cwd+"/augmented/masks/") == True and len(os.listdir(cwd+"/augmented/masks/")) == 2
+    assert os.path.exists(
+        cwd+"/augmented/obj_det_label/") == True and len(os.listdir(cwd+"/augmented/obj_det_label/")) == 2
--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -3,8 +3,9 @@ import numpy as np
 import glob
 import os
 
-def resize_images(folder_path,image_dimension):
-    extensions = ("*.png","*.jpg","*.jpeg",)
+
+def resize_images(folder_path, image_dimension):
+    extensions = ("*.png", "*.jpg", "*.jpeg",)
     files = []
     for extension in extensions:
         files.extend(glob.glob(folder_path+"/"+extension))
@@ -15,25 +16,33 @@ def resize_images(folder_path,image_dimension):
         extension = f.split('/')[-1]
         cv2.imwrite(folder_path+'/temp/'+extension, res)
 
-def rename_images_labels(labels_folder,images_folder,labels_file_path):
+
+def rename_images_labels(labels_folder, images_folder, labels_file_path):
     labels = ['background']
     labels_txt_file = open(labels_file_path)
     for i in labels_txt_file.readlines():
-    	if i.rstrip()not in['__ignore__','_background_']:
-    		labels.append(i.rstrip())
+        if i.rstrip()not in['__ignore__', '_background_']:
+            labels.append(i.rstrip())
     images_files = glob.glob(images_folder+"*")
     images_files.sort()
     labels_files = glob.glob(labels_folder+"*")
     labels_files.sort()
     class_count = np.zeros(len(labels)).astype(np.float)
-    for i, j in zip(images_files,labels_files):
+    for i, j in zip(images_files, labels_files):
         label_image = cv2.imread(j)
         head, tail = os.path.split(i)
-        class_count[np.max(label_image)]+=1
-        new_name = labels[np.max(label_image)]+"_"+"%04d" % class_count[np.max(label_image)]+"."+tail.split('.')[-1]
-        os.rename(i,head+"/"+new_name)
-        os.rename(j,os.path.split(j)[0]+"/"+new_name)
+        class_count[np.max(label_image)] += 1
+        new_name = labels[np.max(label_image)]+"_" + \
+            "%04d" % class_count[np.max(label_image)]+"."+tail.split('.')[-1]
+        os.rename(i, head+"/"+new_name)
+        os.rename(j, os.path.split(j)[0]+"/"+new_name)
 
 
 def rename_backgrounds(backgrounds_folder):
-    pass
+    backgrounds_files = glob.glob(backgrounds_folder+"*")
+    for i, j in enumerate(backgrounds_files):
+        label_image = cv2.imread(j)
+        head, tail = os.path.split(j)
+        new_name = "background_" + \
+            "%04d" % i+"."+tail.split('.')[-1]
+        os.rename(j, head+"/"+new_name)
